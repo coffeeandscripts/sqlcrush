1a6bb0f9acbb0be106b46963e722e6fd94ee5746
traverseda <traverse.da@gmail.com>
2017-12-06 13:09:58 -0400

    Added pypy support for setup.

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup
+import platform, warnings
+
+#Pypy dependency support
+python_implementation = platform.python_implementation()
+
+install_requires = ['sqlalchemy', 'pymysql', 'psycopg2',]
+if python_implementation == "PyPy":
+    install_requires = ['sqlalchemy', 'pymysql', 'psycopg2cffi',]
+elif python_implementation != "CPython":
+    warnings.warn("We don't know how to deal with the {} runtime. Treating it like CPython".format(python_implementation), RuntimeWarning)
 
 setup(name='sqlcrush',
         version='0.1.5',
@@ -9,6 +19,6 @@ setup(name='sqlcrush',
         license='GNU',
         scripts=['bin/sqlcrush',],
         packages=['sqlcrush',],
-        install_requires=['sqlalchemy', 'pymysql', 'psycopg2',],
+        install_requires=install_requires,
         include_package_data=True
 )
